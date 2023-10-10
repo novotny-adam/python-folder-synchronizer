e5d48e4e200b836691c9d5d5f927d3b11d67e13d
Adam Novotný <novotny.adam@protonmail.com>
2023-10-10 09:52:38 +0200

    inital logic and file structure

---
+++ python-folder-synchronizer.py
@@ -0,0 +1,68 @@
+import os 
+import shutil
+import logging
+import hashlib # Library for comparison of files by md5 hash
+
+# Logging to file
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.INFO, 
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+SYNC_INTERVAL = 300 # Time in seconds
+SOURCE_FOLDER_PATH = "source/"
+REPLICA_FOLDER_PATH = "replica/"
+
+def copySourceEntry(sourceEntryPath):
+  try:
+    destinationPath = createDestinationPath(sourceEntryPath)
+    if os.path.isfile(sourceEntryPath):
+      shutil.copy(sourceEntryPath, destinationPath)
+    elif os.path.isdir(sourceEntryPath):
+      shutil.copytree(sourceEntryPath, destinationPath)
+    logging.info(f"Copied: {sourceEntryPath} to {destinationPath}")
+    print(f"Copied: {sourceEntryPath} to {destinationPath}")
+  except Exception as e: 
+        logging.error(f"Error copying {sourceEntryPath} to {destinationPath}: {str(e)}")
+        print(f"Error: {str(e)}")
+
+def deleteDestinationEntry(destinationEntryPath):
+  try:
+    if os.path.isfile(destinationEntryPath):
+      os.remove(destinationEntryPath)
+    elif os.path.isdir(destinationEntryPath):
+      shutil.rmtree(destinationEntryPath)
+    logging.info(f"Deleted: {destinationEntryPath}")
+    print(f"Deleted: {destinationEntryPath}")
+  except Exception as e: 
+        logging.error(f"Error deleting {destinationEntryPath}: {str(e)}")
+        print(f"Error: {str(e)}")
+
+def createDestinationPath(sourcePath):
+  return sourcePath.replace(SOURCE_FOLDER_PATH, REPLICA_FOLDER_PATH)
+
+def createSourcePath(destinationPath):
+   return destinationPath.replace(REPLICA_FOLDER_PATH, SOURCE_FOLDER_PATH)
+
+
+
+def compareFolderContent():
+  # Copying files and folderss to replica directory, if new file or folder will appear in source directory
+  for root, dirs, files in os.walk(SOURCE_FOLDER_PATH):
+     allEntries = dirs + files
+     for entryName in allEntries:
+        entryPath = os.path.join(root, entryName)
+        destinationEntryPath = createDestinationPath(entryPath)
+        if not os.path.exists(destinationEntryPath):
+          copySourceEntry(entryPath)
+  # Deleting files and folders, if there not anymore in source folder
+  for root, dirs, files in os.walk(REPLICA_FOLDER_PATH):
+     allEntries = dirs + files
+     for entryName in allEntries:
+        destinationEntryPath = os.path.join(root, entryName)
+        sourceEntryPath = createSourcePath(destinationEntryPath)
+        if not os.path.exists(sourceEntryPath):
+           deleteDestinationEntry(destinationEntryPath)
+
+
+compareFolderContent()
+
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Python folder synchronizer
+
+This program synchronize two folders. The main folder is source and it is moving files to replica.
